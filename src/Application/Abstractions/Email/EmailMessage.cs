578a94ec14a3c8f8c7c2da2be1a189dceb023f4a
Hasibur Rahman Fayez <hasiburrahmanf7@gmail.com>
2026-05-15 00:50:59 +0600

    Clean main branch - remove all code

--- src/Application/Abstractions/Email/EmailMessage.cs
+++
@@ -1,9 +0,0 @@
-namespace Application.Abstractions.Email;
-
-public sealed class EmailMessage
-{
-    public required string To { get; init; }
-    public required string Subject { get; init; }
-    public required string Body { get; init; }
-    public bool IsHtml { get; init; } = true;
-}
--- src/Application/Portfolios/Delete/DeletePortfolioCommand.cs
+++
@@ -1,5 +0,0 @@
-﻿using Application.Abstractions.Messaging;
-
-namespace Application.Portfolios.Commands;
-
-public sealed record DeletePortfolioCommand(Guid PortfolioId) : ICommand;
