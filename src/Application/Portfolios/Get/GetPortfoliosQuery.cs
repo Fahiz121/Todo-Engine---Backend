e0a0678657702421ee8228c26ad00748c9465802
Hasibur Rahman Fayez <hasiburrahmanf7@gmail.com>
2026-05-15 23:47:37 +0600

    Removed Files

--- src/Application/Portfolios/Get/GetPortfoliosQuery.cs
+++
@@ -1,5 +0,0 @@
-﻿using Application.Abstractions.Messaging;
-
-namespace Application.Portfolios.Queries;
-
-public sealed record GetPortfoliosQuery(bool IsFeatured) : IQuery<List<PortfolioResponse>>;
--- src/Application/Portfolios/GetById/GetPortfolioByIdQuery.cs
+++
@@ -1,5 +0,0 @@
-﻿using Application.Abstractions.Messaging;
-
-namespace Application.Portfolios.Queries;
-
-public sealed record GetPortfolioByIdQuery(Guid PortfolioId, bool IsFeatured) : IQuery<PortfolioResponse>;
